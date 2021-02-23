dce9bb0ead81bd84e7f6ecd315175548aa9de0fd
robinlin <rr999888tw@gmail.com>
2021-02-23 16:50:18 +0800

    next: find keybindings for lock screen

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 2. Install common applications (curl, git, zsh, oh-my-zsh, vim, node, snapd, build-essential)
 3. Install chrome, skype, vscode
 4. Install Chinese keyboard typing
-5. Install gnome-shell-extensions
+5. Install gnome-shell-extensions (install gnome-tweaks, chrome-gnome-shell, and the extensions)
 6. Set my customized keybindings
 7. Set my default terminal fonts and size
--- my-keybindings-script.sh
+++ my-keybindings-script.sh
@@ -2,13 +2,28 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Contro
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Control><Super>Down', '<Control><Super>j']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Super>Left', '<Control><Super>h']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Super>Right', '<Control><Super>l']"
-echo '[+] set the keybindings to allow workspace switching with Control+Super+(h,j,k,l)'
+echo '[+] added keybindings to allow workspace switching with Control+Super+(h,j,k,l)'
 
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Up', '<Super><Shift>k']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Down', '<Super><Shift>j']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super><Shift>Left', '<Super><Shift>h']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super><Shift>Right', '<Super><Shift>l']"
-echo '[+] set the keybindings to allow windowing moving to another workspace with Super+Shift+(h,j,k,l)'
+echo '[+] added keybindings to allow windowing moving to another workspace with Super+Shift+(h,j,k,l)'
 
-gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>m', '<Super>Up']"
-echo '[+] added Super+m to toggle the maximization of windows'
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-up "['<Control><Alt>Up', '<Control><Alt>k']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Control><Alt>Right', '<Control><Alt>l']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Control><Alt>Left', '<Control><Alt>h']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-down "['<Control><Alt>Down', '<Control><Alt>j']"
+echo '[+] added keybindings to allow windows moving to other monitors with Contrl+Alt+(arrow keys/h,j,k,l)'
+
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right', '<Super>l']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Right', '<Super>h']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Super>Up', '<Super>k']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>Down', '<Super>j']"
+echo '[+] added keybindgs to allow window tiling with Super+(arrow keys/hjkl)'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>t', '<Control><Alt>t']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>b']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>f']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys email "['<Super>e']"
+echo '[+] added shortcuts to launch applications, inlcuding Browser, File manager, email client, terminal'
